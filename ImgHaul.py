0323baae6b8b1569ab644a7b8d4ecfedc88ecd2f
danylolev <danylolev@gmail.com>
2023-04-28 15:00:53 -0400

    finished ImgHaul.py

--- ImgHaul.py
+++ ImgHaul.py
@@ -1,11 +1,10 @@
-#TODO: Add copy as lightweight JPEG frame
-#TODO: Check input validation
+#TODO: optimize exif so it's not slow
 
 
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image
-import shutil, os, exifread, time
+import shutil, os, exifread
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -35,9 +34,30 @@ def remove_files():
     genPyplot()
 
 
-
 def get_destination():
     dest_path.set(filedialog.askdirectory())
+
+
+def folderLockToggle():
+    if folderEState.get() == "disabled":
+        folderE.config(state="normal")
+        folderEState.set("normal")
+    else:
+        folderE.delete(0, END)
+        folderE.insert(0, "New Folder")
+        folderE.config(state="disabled")
+        folderEState.set("disabled")
+        
+
+def renameLockToggle():
+    if prefixState.get() == "disabled":
+        prefix.config(state="normal")
+        prefixState.set("normal")
+    else:
+        prefix.delete(0, END)
+        prefix.insert(0, "Select a prefix")
+        prefix.config(state="disabled")
+        prefixState.set("disabled")
 
 
 def Error(msg:str, win:Tk):
@@ -69,39 +89,54 @@ def success():
 
 def genPyplot():
     fig = Figure(figsize = (6,4))
+    fig.set_facecolor("gray")
     mm = {}
 
     for file in paths:
         f = open(file, 'rb')
-        tags = exifread.process_file(f)
-        if len(tags) == 0:
-            break
-        elif int(str(tags["EXIF FocalLength"])) not in mm.keys():
-            mm[int(str(tags["EXIF FocalLength"]))] = 1
-        else:
-            mm[int(str(tags["EXIF FocalLength"]))] += 1
+        tags = exifread.process_file(f, stop_tag='EXIF FocalLength')
+        # if len(tags) == 0:
+        #     break
+        # elif int(str(tags["EXIF FocalLength"])) not in mm.keys():
+        #     mm[int(str(tags["EXIF FocalLength"]))] = 1
+        # else:
+        #     mm[int(str(tags["EXIF FocalLength"]))] += 1
+        focalLength = tags.get("EXIF FocalLength")
+        focalLength = str(focalLength)
+        if focalLength != None:
+            if "/" in focalLength:
+                num, denom = str(focalLength).split("/")
+                focalLength = float(num) / float(denom)
+            if focalLength not in mm.keys():
+                mm[focalLength] = 1
+            else:
+                mm[focalLength] += 1
 
     plot = fig.add_subplot(111)
-
+    print(type(list(mm.keys())[0]))
+    
     xticks = list(sorted(mm.keys()))
-    plot.bar(mm.keys(), mm.values())
+    plot.bar(mm.keys(), mm.values(), color="white")
     plot.set_xticks(xticks)
     plot.set_xticklabels(xticks)
+    plot.tick_params(colors="white")
 
-    plot.set_xlabel("Focal Length in mm", fontsize=12)
-    plot.set_ylabel("Occurrences", fontsize=12)
-    plot.set_title("Focal Length Distribution", fontsize=16)
+    plot.set_xlabel("Focal Length in mm", fontsize=12, color="white")
+    plot.set_ylabel("Occurrences", fontsize=12, color="white")
+    plot.set_title("Focal Length Distribution", fontsize=16, color="white")
+    plot.spines[:].set_color("white")
+    plot.set_facecolor("gray")
 
     if not mm:
         plot.set_yticks([1])
     else:
         plot.yaxis.set_major_locator(MaxNLocator(integer=True))
 
-    frame = Frame(win)
+    frame = Frame(win,border=3,relief=GROOVE)
     canvas = FigureCanvasTkAgg(fig,master=frame)
     canvas.draw()
     canvas.get_tk_widget().pack()
-    frame.grid(row=0, column=1, pady=10, sticky=W)
+    frame.grid(row=0, column=0, padx=(300,15) ,pady=10, sticky=W)
 
 # Check if file with name already exists and throw error to chose another name
 def move(win):
@@ -195,12 +230,9 @@ win.title("ImgHaul")
 win.configure(background="gray")
 win.resizable(False, False)
 
-### Quit Button ###
-quitButton = Button(win, text="Quit", command=win.quit)
-quitButton.grid(row=3, column=3,sticky=E)
-### Go Button ###
-goButton = Button(win, text="Go", command=lambda: go(win))
-goButton.grid(row=3,column=2,sticky=E)
+
+##### Main Frame #####
+mf = Frame(win, bg="gray")
 
 
 ### Files Listbox ###s
@@ -208,44 +240,47 @@ filesFrame = LabelFrame(win, text="Files", bg="gray")
 filesLb = Listbox(filesFrame, width=30, height=20, selectmode=MULTIPLE, bg="dark gray", relief="sunken", bd=1)
 filesLb.pack(side=TOP)
 # Directory Button #
-dirButton = Button(filesFrame, text="Browse", command=get_files)
+dirButton = Button(filesFrame, text="Browse", highlightbackground="gray", highlightcolor="gray", command=get_files)
 dirButton.pack(side=LEFT)
 # Remove Button #
-removeButton = Button(filesFrame, text="Remove Files", command=remove_files)
+removeButton = Button(filesFrame, text="Remove Files", highlightbackground="gray", highlightcolor="gray", command=remove_files)
 removeButton.pack(side=RIGHT)
-filesFrame.grid(row=0, column=0, padx=10)
+filesFrame.grid(row=0, column=0, padx=10, sticky=W)
 
 
 ### Destination Settings ###
-Destination = LabelFrame(win, width=40, text="Destination", bg="gray")
+Destination = LabelFrame(mf, width=40, text="Destination", bg="gray")
 dest_path = StringVar(Destination, value="")
-destInp = Label(Destination, textvariable=dest_path, relief=RAISED, bg="gray", width=20)
-destBrowse = Button(Destination, text="Browse", command=get_destination)
+destInp = Label(Destination, textvariable=dest_path, width=20)
+destBrowse = Button(Destination, text="Browse", highlightbackground="gray", highlightcolor="gray", command=get_destination)
 # New Folder Settings #
 folderVal = IntVar()
-folderCB = Checkbutton(Destination, variable=folderVal, text="Make New Folder", bg="gray")
+folderCB = Checkbutton(Destination, variable=folderVal, text="Make New Folder", bg="gray", command=folderLockToggle)
 folderName = StringVar(Destination, value="New Folder")
-folderE = Entry(Destination, textvariable=folderName)
+folderEState = StringVar()
+folderEState.set("disabled")
+folderE = Entry(Destination, highlightbackground="gray", highlightcolor="gray", state="disabled", textvariable=folderName)
 folderE.pack(side=BOTTOM)
 folderCB.pack(side=BOTTOM)
 destInp.pack(side=LEFT)
 destBrowse.pack(side=RIGHT)
-Destination.grid(row=2, column=0, padx=10, sticky=W)
+Destination.grid(row=0, column=0)
 
 ### Rename Settings ###
-renameFrame = LabelFrame(win, text="Rename", bg="gray")
+renameFrame = LabelFrame(mf, text="Rename", bg="gray")
 renameVal = IntVar()
 prefixVar = StringVar()
-renameCB = Checkbutton(renameFrame, bg="gray", text="Rename Files", variable=renameVal)
-prefix = Entry(renameFrame, textvariable=prefixVar)
-prefixLabel = Label(renameFrame, bg="gray", text="Select a prefix")
+prefixVar.set("Select a prefix")
+renameCB = Checkbutton(renameFrame, bg="gray", text="Rename Files", variable=renameVal, command=renameLockToggle)
+prefixState = StringVar()
+prefixState.set("disabled")
+prefix = Entry(renameFrame, highlightbackground="gray", highlightcolor="gray", state="disabled", textvariable=prefixVar)
 renameCB.pack(side=TOP)
 prefix.pack(side=LEFT)
-prefixLabel.pack(side=RIGHT)
-renameFrame.grid(row=2, column=1, padx=(140,0), sticky=W)
+renameFrame.grid(row=0, column=1, padx=10)
 
 ### Copy as JPEG ###
-jpegFrame = LabelFrame(win, text="Copy as JPEG", bg="gray")
+jpegFrame = LabelFrame(mf, text="Copy as JPEG", bg="gray")
 quality = IntVar()
 jpegVal = IntVar()
 asJpeg = Checkbutton(jpegFrame, bg="gray", text="Copy files as JPEGs", variable=jpegVal)
@@ -254,7 +289,19 @@ qualSlider = Scale(jpegFrame, bg="gray", orient= HORIZONTAL, variable=quality, f
 asJpeg.pack(side=TOP)
 qualLabel.pack(side=BOTTOM)
 qualSlider.pack(side=BOTTOM)
-jpegFrame.grid(row=2, column=2)
+jpegFrame.grid(row=0, column=2)
+
+### Go Button ###
+goButton = Button(mf, text="Go", highlightbackground="gray", highlightcolor="gray", command=lambda: go(win))
+goButton.grid(row=0,column=3, padx=(80,0), sticky=N, pady=(30,0))
+
+### Quit Button ###
+quitButton = Button(mf, text="Quit", bg="gray", highlightbackground="gray", highlightcolor="gray", command=win.quit)
+quitButton.grid(row=0, column=3, pady=(50,5), padx=(80,0))
+
+
+
+mf.grid(row=1, column=0, pady=(0,10))
 
 genPyplot()
 
